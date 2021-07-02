2c6d3bf9e68ec3e0b8cc66b49d79dd123f7c0b7e
John Brander <john.brander@june.se>
2021-07-02 10:58:02 +0200

    Update dbio-publicversion-main-sql-create-db-users.ps1

--- dbio-publicversion-main-sql-create-db-users.ps1
+++ dbio-publicversion-main-sql-create-db-users.ps1
@@ -1,32 +1,37 @@
-﻿                
-$userPassword = ConvertTo-SecureString -String ${Env:myPass} -AsPlainText -Force
-$dbLoginPassword = ConvertTo-SecureString -String ${Env:myPass} -AsPlainText -Force
-$Credential = New-Object System.Management.Automation.PSCredential (${Env:dbUserName}, $userPassword)
+$administratorLoginPassword = ConvertTo-SecureString -String ${Env:administratorLoginPassword} -AsPlainText -Force
+$adminCredential = New-Object System.Management.Automation.PSCredential (${Env:administratorLogin}, $administratorLoginPassword)
+
+$dbUserLogin = ${Env:dbUserLogin}
+$dbUserLoginPassword = ConvertTo-SecureString -String ${Env:dbUserLoginPassword} -AsPlainText -Force
 
 Write-Output 'Hello from the ARM Template deployment'
-Write-Output ${Env:myPass}
-Write-Output ${Env:dbUserName}
+Write-Output $administratorLogin
+Write-Output $administratorLoginPassword
+Write-Output $administratorLogin
+Write-Output $administratorLoginPassword
+Write-Output $dbUserLogin
+Write-Output $dbUserLoginPassword
 Write-Output ${Env:sqlInstance}
 
 Install-Module dbatools -force
 
 New-DbaLogin -SqlInstance ${Env:sqlInstance} `
--SqlCredential $Credential `
--Login $dbUserName `
--SecurePassword $dbLoginPassword `
+-SqlCredential $adminCredential `
+-Login $dbUserLogin `
+-SecurePassword $dbUserLoginPassword `
 -Force
  
 New-DbaDbUser -SqlInstance ${Env:sqlInstance} `
 -Database acq `
--SqlCredential $Credential `
--Login $dbUserName `
+-SqlCredential $adminCredential `
+-Login $dbUserLogin `
 -Force
 
 Add-DbaDbRoleMember -SqlInstance ${Env:sqlInstance} `
 -Database acq `
--SqlCredential $Credential `
+-SqlCredential $adminCredential `
 -Role db_owner `
--User  $dbUserName `
+-User  $dbUserLogin `
 -Confirm: $false  
 
 
